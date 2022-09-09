bfa48573391ad12e91f854d53d6b37536dd47361
Cyborgalx <omotoshoajoke1122@gmail.com>
2022-09-08 17:05:04 -0700

    Trial

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * Description: Write a c programme that prints using puts function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
