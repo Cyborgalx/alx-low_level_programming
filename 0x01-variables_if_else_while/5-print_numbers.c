07e8c2b22abe525f24587e6b5585093f792afc0e
Cyborgalx <omotoshoajoke1122@gmail.com>
2022-09-11 14:13:42 -0700

    Trial

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - ENtry point
+ * Description:  a program that prints all sinle digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char d;
+
+	for (d =  '0'; d <= '9'; d++)
+
+	putchar(d);
+
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - ENtry point
+ * Description: Print number 1 - 9
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char k;
+
+	for (k = '0'; k <= '9'; k++)
+	{
+	putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
