4e67c31e831b8396ca019c338b985b344bf5f016
Cyborgalx <omotoshoajoke1122@gmail.com>
2022-09-13 16:45:07 -0700

    Trial

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,11 +10,11 @@ int main(void)
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, sz;
 
-	sz = sizeof(str) / sizof(int);
+	sz = sizeof(str) / sizeof(int);
 	for (count = 0; count < sz; count++)
 	{
-	_puutchar(str[count]);
+	_putchar(str[count]);
 	}
-	putgchar('\n');
+	putchar('\n');
 	return (0);
 }
