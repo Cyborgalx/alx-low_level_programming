ebb21adca7fb2c158e8293ff64728391b3cde47e
Cyborgalx <omotoshoajoke1122@gmail.com>
2022-09-13 17:41:44 -0700

    Trial

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Programme to print alphabet
+ * Description: a function that prints the alphabet.
+ * Return: ALways 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char f;
+
+	for (f = 'a'; f <= 'z'; f++)
+	_putchar(f);
+
+	putchar('\');
+	return (0);
+}
