389a4e3cc4b279ef461f40eb9519be49d9fdd1aa
Cyborgalx <omotoshoajoke1122@gmail.com>
2022-09-14 14:04:39 -0700

    Trial

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,18 +12,18 @@ int main(void)
 
 	while (i < 50)
 	{
-		if (i == 0)
-			printf("%ld", j);
-		else if (i == 1)
-			printf(", %ld", k);
-		else
-		{
-			k += j;
-			j = k - j;
-			printf(", %ld" k);
-		}
+	if (i == 0)
+	printf("%ld", j);
+	else if (i == 1)
+	printf("%ld", k);
+	else
+	{
+	k += j;
+	j = k - j;
+	printf("%ld" k);
+	}
 
-		++i;
+	++i;
 	}
 
 	printf("\n");
