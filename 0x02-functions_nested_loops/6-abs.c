878a234b47d538fbf1cf11d05c2e57dbc2a84c59
Cyborgalx <omotoshoajoke1122@gmail.com>
2022-09-14 03:51:57 -0700

    Trial

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "stdio.main"
+
+/**
+ * _abs - Computes the absolute value of integer
+ * @n: The interger to be commited.
+ *
+ * Return: The absolute value of the integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	return (n);
+	else
+	return (-n);
+}
